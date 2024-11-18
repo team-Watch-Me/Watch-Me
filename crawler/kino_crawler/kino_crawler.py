1f4b4648feb5dda300464c4cca793aea1a1158f8
HyoKyuKang <djs080307@gmail.com>
2024-11-18 20:22:18 +0900

    add

--- crawler/kino_crawler/kino_crawler.py
+++ crawler/kino_crawler/kino_crawler.py
@@ -43,7 +43,8 @@ class KinoCrawler:
             
             
             data[content_name] = contents
-            required_fields = ["content_name", "streaming_provider", "plot", "genre", "age_rating", "country", "release_date", "running_time", "year" ]
+            required_fields = self.fields
+            #required_fields = ["content_name", "streaming_provider", "plot", "genre", "age_rating", "country", "release_date", "running_time", "year" ]
             filtered_data = {
                 key: value
                 for key, value in data.items()
