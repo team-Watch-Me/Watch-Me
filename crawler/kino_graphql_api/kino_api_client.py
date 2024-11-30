f248378690c522f0eee6ba147cc844954918a466
SNL0915 <nicknvr@naver.com>
2024-11-30 18:33:56 +0900

    [feat] add queries and changed the overall structure of the code

--- crawler/kino_graphql_api/kino_api_client.py
+++ crawler/kino_graphql_api/kino_api_client.py
@@ -4,19 +4,19 @@ import json
 
 class KinoAPIClient:
 
-    def __init__(self):
-        self.kino_json = open('./kino.json', 'r', encoding='utf-8')
+    def __init__(self, type):
+        self.kino_json = open('kino_query.json', 'r', encoding='utf-8')
         self.kino = json.load(self.kino_json)
-        self.url = self.kino["url"]
-        self.headers = self.kino["headers"]
-        self.data = self.kino["data"]
+        self.url = self.kino[type]['url']
+        self.headers = self.kino[type]["headers"]
+        self.data = self.kino[type]["data"]
 
     def make_request(self, movie_id):
         self.data["variables"]["movieId"] = movie_id
         response = requests.post(self.url, headers=self.headers, json=self.data)
 
         if response.status_code == 200:
-            print(response.json())
+            return response.json()
         else:
             print(f"fail: {response.status_code}")
             print(response.text)
